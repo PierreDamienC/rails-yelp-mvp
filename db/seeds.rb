a844bd01584a46b9094f5a678e65950d415e93ff
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-11-18 08:07:06 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Adding 5 restaurants..."
+Restaurant.create!(name:"Restaurant 1", address:"Lille", category:"chinese", phone_number:"00.00.00.00.00")
+Restaurant.create!(name:"Restaurant 2", address:"Paris", category:"italian", phone_number:"00.00.00.00.00")
+Restaurant.create!(name:"Restaurant 3", address:"Marseille", category:"japanese", phone_number:"00.00.00.00.00")
+Restaurant.create!(name:"Restaurant 4", address:"Lyon", category:"french", phone_number:"00.00.00.00.00")
+Restaurant.create!(name:"Restaurant 5", address:"Bordeaux", category:"belgian", phone_number:"00.00.00.00.00")
+puts "Done"
